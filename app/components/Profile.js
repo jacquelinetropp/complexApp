b383fb2b72447656a3361c909e18ae7e3337f3ed
travelingtropp <48297118+travelingtropp@users.noreply.github.com>
2020-05-20 17:39:27 -0500

    first push, error on single post screen

---
+++ app/components/Profile.js
@@ -0,0 +1,64 @@
+import React, { useEffect, useContext, useState } from "react";
+import { useParams } from "react-router-dom";
+
+import Axios from "axios";
+import StateContext from "../StateContext";
+
+import Page from "./Page";
+import ProfilePosts from "./ProfilePosts";
+
+const Profile = (props) => {
+  const appSate = useContext(StateContext);
+  const [profileData, setProfileData] = useState({
+    profileUsername: "...",
+    profileAvatar: "https://gravatar.com/avatar/placeholder?s=128",
+    isFollowing: false,
+    counts: {
+      postCount: "",
+      follwerCount: "",
+      follwingCount: "",
+    },
+  });
+
+  const { username } = useParams();
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await Axios.post(`/profile/${username}`, {
+          token: appSate.user.token,
+        });
+        setProfileData(response.data);
+      } catch (e) {
+        console.log("there was a problem");
+      }
+    }
+    fetchData();
+  }, []);
+  return (
+    <Page title="Profile">
+      <h2>
+        <img className="avatar-small" src={profileData.profileAvatar} />{" "}
+        {profileData.profileUsername}
+        <button className="btn btn-primary btn-sm ml-2">
+          Follow <i className="fas fa-user-plus"></i>
+        </button>
+      </h2>
+
+      <div className="profile-nav nav nav-tabs pt-2 mb-4">
+        <a href="#" className="active nav-item nav-link">
+          Posts: {profileData.counts.postCount}
+        </a>
+        <a href="#" className="nav-item nav-link">
+          Followers: {profileData.counts.follwerCount}
+        </a>
+        <a href="#" className="nav-item nav-link">
+          Following: {profileData.counts.follwingCount}
+        </a>
+      </div>
+
+      <ProfilePosts />
+    </Page>
+  );
+};
+
+export default Profile;
---
+++ app/components/ViewSinglePost.js
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
+import { useParams, Link } from "react-router-dom";
+import Page from "./Page";
+
+const ViewSinglePost = (props) => {
+  const { id } = useParams();
+  const [isLoading, setIsLoading] = useState(true);
+  const [post, setPost] = useState();
+
+  useEffect(() => {
+    async function fetchPost() {
+      try {
+        const response = await Axios.get(`/post/${id}`);
+        setPost(response.data);
+        setIsLoading(false);
+      } catch (e) {
+        console.log("There was a problem.");
+      }
+    }
+    fetchPost();
+  }, []);
+
+  if (isLoading)
+    return (
+      <Page title="...">
+        <div>Loading...</div>
+      </Page>
+    );
+
+  const date = new Date(post.createdDate);
+  const dateFormatted = `${
+    date.getMonth() + 1
+  }/${date.getDate()}/${date.getFullYear()}`;
+  return (
+    <Page title="">
+      <div className="d-flex justify-content-between">
+        <h2>{post.title}</h2>
+        <span className="pt-2">
+          <a href="#" className="text-primary mr-2" title="Edit">
+            <i className="fas fa-edit"></i>
+          </a>
+          <a className="delete-post-button text-danger" title="Delete">
+            <i className="fas fa-trash"></i>
+          </a>
+        </span>
+      </div>
+      <p className="text-muted small mb-4">
+        <a href="#">
+          <img className="avatar-tiny" src={post.author.avatar} />
+        </a>
+        Posted by <a href="#">{post.author.username}</a> on {dateFormatted}
+      </p>
+      <div className="body-content">{post.body}</div>;
+    </Page>
+  );
+};
+
+export default ViewSinglePost;
